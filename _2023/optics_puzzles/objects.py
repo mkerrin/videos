c6c915607541e2161454b2b871f324a8fd9f6948
Michael Kerrin <michael.kerrin@gmail.com>
2025-02-21 00:21:17 +0000

    Override TimeVaryingVectorField to work with the VectorField coordinates

--- _2023/optics_puzzles/objects.py
+++ _2023/optics_puzzles/objects.py
@@ -724,6 +724,39 @@ class OscillatingFieldWave(GraphAsVectorField):
         return self.wave.offset + self.axes.x_axis.n2p(self.sample_xs)
 
 
+# Alternatives - override manim-original TimeVaryingVectorField to
+# work with e_field.py
+
+class TimeVaryingVectorField(TimeVaryingVectorField):
+
+    def __init__(
+            self,
+            func,
+            center=ORIGIN,
+            x_density=2.0,
+            y_density=2.0,
+            z_density=2.0,
+            width=14,
+            height=8,
+            depth=0,
+            **kwargs
+            ):
+        # Equivalent to get_sample_points(center, width, height, depth,
+        #     x_density, y_density, z_density
+        # )
+        spacings = 1.0 / np.array([x_density, y_density, z_density])
+        coords = ThreeDAxes(
+            x_range=(-width / 2, width / 2, spacings[0]),
+            y_range=(-height / 2, height / 2, spacings[1]),
+            z_range=(-depth / 2, depth / 2, spacings[2]),
+        )
+        super().__init__(
+            func,
+            coordinate_system=coords, density=1.0,
+            **kwargs
+        )
+
+
 # Structure
 
 
